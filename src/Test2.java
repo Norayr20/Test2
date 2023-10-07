4a08ec6551fa22f2aa5db6cd5312980111fcb314
Norayr <asoyannorayr99@gmail.com>
2023-10-07 13:20:50 +0300

    add Test2

---
+++ src/Test2.java
@@ -0,0 +1,12 @@
+public class Test2 {
+    public static void main(String[] args) {
+        int varInt = 20;
+        byte varByte = -1;
+        short varShort = 10;
+        long varLong = -100L;
+        double varDouble = -11.1;
+        float varFloat = 11.1F;
+        char varChar = 2000;
+        boolean varBoolean = false;
+    }
+}
